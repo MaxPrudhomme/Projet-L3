eb87987b90f7d186381a3bfef4af7dfcd7e5bc9a
Max Prudhomme <account@maxprudhomme.com>
2024-03-28 14:05:00 +0100

    Widget Browser v.1
    First widget browser implementation. Adding and removing placeholder widgets functional.

---
+++ src/lib/content/InteractionContainer.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { editMode } from "../../store"
+    import WidgetBrowser from "./WidgetBrowser.svelte";
+</script>
+
+<div id="container" class="absolute">
+    {#if $editMode}
+        <WidgetBrowser></WidgetBrowser>
+    {/if}
+</div>
+
+<style>
+#container {
+    height: 820px;
+    width: 1800px;
+}
+</style>
--- src/lib/content/Widget.svelte
+++ src/lib/content/Widget.svelte
@@ -1,14 +1,44 @@
 <script>
+    import { onMount } from 'svelte';
+    
+    export let height = null;
+    export let width = null;
+    export let disabled = false;
+
     let containerRef;
+    let blockerRef;
+    
+    onMount(() => {
+        if (height !== null && width !== null) {
+            containerRef.style.height = height + 'px';
+            containerRef.style.width = width + 'px';
+            blockerRef.style.height = height + 'px';
+            blockerRef.style.width = width + 'px';
+        }
+    });
 </script>
 
-<div id="container" bind:this={containerRef}></div>
+<div id="container" bind:this={containerRef}>
+    {#if disabled}
+        <div id="blocker" bind:this={blockerRef} style="visibility: visible;"></div>
+    {:else}
+        <div id="blocker" bind:this={blockerRef} style="visibility: hidden;"></div>
+    {/if}
+</div>
 
 <style>
     #container {
+        display: inline-flex;
+        position: relative;
         background-color: rgba(0, 0, 0, 0.3);
         border-radius: 20px;
         height: 100%;
         width: 100%
     }
+
+    #blocker {
+        border-radius: 20px;
+        cursor: pointer;
+    }
+
 </style>
