47a32fbc0d273fa6407d930372416e13dde07ddd
MrSc33nsh0t <76527824+MrSc33nsh0t@users.noreply.github.com>
2024-04-03 15:59:19 +0200

    Arrow changing in MarkItem

--- src/lib/widgets/MarkItem.svelte
+++ src/lib/widgets/MarkItem.svelte
@@ -5,11 +5,15 @@
 		'M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm8.5 2.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293z';
 	let arrowUpPath =
 		'M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm8.5 9.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z';
+
+	let arrowPath = arrowUpPath;
 </script>
 
 <div id="container">
 	<h1 id="mark">{mark}</h1>
 	<h2>/20</h2>
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
 	<svg
 		xmlns="http://www.w3.org/2000/svg"
 		width="50"
@@ -17,8 +21,12 @@
 		fill="currentColor"
 		class="bi bi-arrow-up-square"
 		viewBox="0 0 16 16"
+		on:click={() => {
+			if (arrowPath == arrowDownPath) arrowPath = arrowUpPath;
+			else arrowPath = arrowDownPath;
+		}}
 	>
-		<path fill-rule="evenodd" d={arrowDownPath} />
+		<path fill-rule="evenodd" d={arrowPath} />
 	</svg>
 </div>
 
