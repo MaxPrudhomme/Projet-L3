17df2a1069d84d96285375d8034a2b42aefabcc5
GenPhoenixx <104349576+GenPhoenixx@users.noreply.github.com>
2024-05-15 15:51:20 +0200

    add function delete in db

--- src/lib/function.js
+++ src/lib/function.js
@@ -14,13 +14,24 @@ async function querydb(startdate, enddate, IDs) {
       body: args
     };
     const response = await fetch(url,options);
-    const data = await response.text();
+    const data = await response.json();
+    data.forEach(e => {
+      e.startDate = new Date(new Date(e.startDate + 'Z'));
+      e.endDate = new Date(new Date(e.endDate + 'Z'))
+      console.log('start:',e.startDate,'/end:',e.endDate);
+    })
     return data;
   } catch (error) {
     console.error("Erreur lors de l'interrogation de la base de données :", error);
     throw error;
   }
 }
+/*
+    const date = new Date('2024-05-03 00:00:00');
+    const utcDate = new Date(date.toISOString().slice(0, 19).replace('T', ' ') + 'Z'); // Ajoute 'Z' pour indiquer que c'est en UTC
+    const localDate = new Date(utcDate); // Convertit en date locale
+    console.log('Date locale:', localDate.toString());
+*/
 /**-------------------------insert-------------------------
 import { querydb } from '../function';
 import { onMount } from 'svelte';
@@ -93,6 +104,38 @@ onMount(async () => {
   }
 });
 */
+async function deletedb(ID) {
+  try {
+    const url = await import.meta.env.VITE_URL + 'delete.php';
+    const args = new FormData();
+    args.append('ID',ID);
+    const options = {
+      method: 'POST',
+      body: args
+    };
+    const response = await fetch(url,options);
+    const data = await response.text();
+    return data;
+  } catch (error) {
+    console.error("Erreur lors de l'interrogation de la base de données :", error);
+    throw error;
+  }
+}
+/**-------------------------insert-------------------------
+import { deletedb } from '../function';
+import { onMount } from 'svelte';
+onMount(async () => {
+  try {
+    const iD = '1ff1f260-cbbc-4b04-a769-a75e6bac80dd' //exemple of ID
+    const data = await deletedb(iD);
+    //you can use data here
+    console.log("Résultat de la fonction PHP :", data);
+  } catch (error) {
+    //handle errors here
+    console.log(error);
+  }
+});
+*/
 /**
      * IDs
      * startDates
@@ -102,4 +145,4 @@ onMount(async () => {
      * locations
      * IDscourse
      */
-export {querydb,insertdb};
+export {querydb,insertdb,deletedb,updatedb};
